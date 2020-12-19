1faae08bc38a58bffe8c6aed90f1cd160d05b88f
Connor Bain <connorbain@u.northwestern.edu>
2020-12-18 22:12:00 -0500

    Add JSON library directory

--- src/LibraryGenerator.py
+++ src/LibraryGenerator.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 
 theLibrary = []
 for root, dirs, files in os.walk("..", topdown=False):
@@ -6,14 +6,13 @@ for root, dirs, files in os.walk("..", topdown=False):
         if name[-7:] == ".ntjson":
             theModel = {"name":name[:-7], "folder":root[3:], "path":os.path.join(root, name)[3:].replace(" ", "%20")}
             theLibrary.append(theModel)
-    
 
 folders = []
 
 for item in theLibrary:
     if item['folder'] not in folders:
         folders.append(item['folder'])
-    
+
 
 folders = sorted(folders, key=str.lower)
 
@@ -21,15 +20,20 @@ NTANGO_PLAYER = "https://netlogoweb.org/ntango-play-standalone?netTangoModel="
 NTANGO_EDITOR = "https://netlogoweb.org/ntango-build?netTangoModel="
 GITHUB_REPO   = "https://raw.githubusercontent.com/NetLogo/nt-models/main/"
 
-f = open("LIBRARY.md", "w")
+f = open(os.path.dirname(__file__) + "../LIBRARY.md", "w")
+libraryDict = []
 f.write("# Preliminary NetTango Models Library Directory\n\n")
-
 for folder in folders:
     f.write("## " + folder + "\n")
     for model in sorted(theLibrary, key = lambda i: i['name']):
         if folder == model['folder']:
-            f.write("""*  [{name}]({playLink}) [(editable)]({editLink})\n\n""".format(name=model['name'], 
+            f.write("""*  [{name}]({playLink}) [(editable)]({editLink})\n\n""".format(name=model['name'],
                                                                   playLink=(NTANGO_PLAYER + GITHUB_REPO + model['path']),
                                                                   editLink=(NTANGO_EDITOR + GITHUB_REPO + model['path'])
                                                                  ))
+            libraryDict.append({ 'name':model['name'], 'url':NTANGO_PLAYER + GITHUB_REPO + model['path'] })
 f.close()
+
+with open(os.path.dirname(__file__) + "../library.json", "w") as outfile:
+    libraryDict = {'success':True, 'models':libraryDict}
+    json.dump(libraryDict, outfile)
